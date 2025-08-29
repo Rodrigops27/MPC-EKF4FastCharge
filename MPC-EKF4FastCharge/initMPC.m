dfcd17093b2b22b82bd4dbe2af9a6dc55b0ed8fe
Rodrigo <A01371600@tec.mx>
2025-08-29 13:50:37 +0200

    Set CRate, comments updated.

--- MPC-EKF4FastCharge/initMPC.m
+++ MPC-EKF4FastCharge/initMPC.m
@@ -1,3 +1,5 @@
+function mpcData = initMPC(SOC0,Np,Nc,targetSOC,Nsim,ROM)
+
 % Establish MPC tuning parameters:
 mpcData.Np = Np;            % Prediction horizon
 mpcData.Nc = Nc;            % Control horizon
@@ -7,14 +9,15 @@ mpcData.Sigma = tril(ones(mpcData.Nc,mpcData.Nc));
 mpcData.Nsim = Nsim;        % Number of simulation points
 
 mpcData.uk_1 = 0;           % Initialization value
-mpcData.SOCk_1 = 0;
+mpcData.SOCk_1 = 0;        
 mpcData.DUk_1 = 0;
 
 mpcData.maxHild = 100;      % Maximum iterations for Hildreth
-mpcData.lambda = [];        % Initialization value
-mpcData.SOC0 = SOC0;
+mpcData.lambda = [];       
+mpcData.SOC0 = SOC0;        % Initialization value
 mpcData.Ts = 1;             % Sampling interval [sec]
 
+Crate = 10; % Max Crate TBD
 Iapp_max = -ROM.cellData.function.const.Q(0.5)*Crate; % Max current
 Iapp_min = 0;
 
@@ -30,6 +33,7 @@ if constraints(2) == 1
     mpcData.const.v_min = V_min;
     mpcData.const.v_max = V_max;
 end
+
 if constraints(3) == 1
     mpcData.const.phise_min = Phise_min;
 end
