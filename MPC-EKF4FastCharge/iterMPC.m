c524846b8130671c1c83bfb1f5388869793e283b
Rodrigo <A01371600@tec.mx>
2025-08-30 21:26:03 +0200

    Constraints and augmentedMatrices wiring updated and tested.

--- MPC-EKF4FastCharge/iterMPC.m
+++ MPC-EKF4FastCharge/iterMPC.m
@@ -13,34 +13,56 @@ function [uk, mpcData] = iterMPC(xk,xk_1,cellState,mpcData)
 Np     = mpcData.Np;
 Nc     = mpcData.Nc;
 uk_1   = mpcData.uk_1;
-Sigma  = mpcData.Sigma;
-Ref    = mpcData.ref*ones(Np,1)/100;
+Sigma  = mpcData.Sigma;  % accumulator (often used in cost/constraints)
+Ref    = mpcData.ref*ones(Np,1)/100; % targetSOC
 
 %% Obtain matrices from Kalman Filter
+% linMatrices = linMat(xk_1,mpcData);
 Am     = cellState.MPC.A;
 Bm     = cellState.MPC.B;
 Csoc   = cellState.MPC.Csoc;
 Dsoc   = cellState.MPC.Dsoc;
 
-% Compute SOC prediction matrices
-[Phi_soc, G_soc] = predMat(Am,Bm,Csoc,Dsoc,Np,Nc);
-
 % Build augmented state vector
-dx = xk - xk_1;
-SOCk_1 = cellState.MPC.SOCk_1;
+% dx = [(xk - xk_1); mpcData.SOCk_1];
+dx = [xk(:); uk_1];   % uk_1 = last applied current
 
-%% Define values needed by Hildreth
-E = -2*G_soc'*(Ref - Phi_soc*dx);
+% Y = Phi*x + G*U
+% Compute SOC prediction matrices
+% y[k+i|k] = C*A^i x_k + sum_{j=0}^{i-1} C*A^{i-1-j}*B * Δu[k+1+j]  + D*u_k
+[Phi_soc, G_soc,Aug_matrices] = predMat(Am,Bm,Csoc,Dsoc,Np,Nc);
+mpcData.Aug_matrices_soc = Aug_matrices;
+
+% Voltage prediction: pass Ctilde=[Cv Dv], D=0
+[Phi_v, G_v,Aug_matrices] = predMat(cellState.MPC.A, cellState.MPC.B, ...
+    cellState.MPC.Cv, 0, Np, Nc);
+mpcData.Aug_matrices_v = Aug_matrices;
+
+% η/ϕ prediction: pass Ctilde=[Cphi Dphi], D=0
+[Phi_phi, G_phi, Aug_matrices] = predMat(cellState.MPC.A, cellState.MPC.B, ...
+    cellState.MPC.Cphi, 0, Np, Nc);
+mpcData.Aug_matrices_phi = Aug_matrices;
+
+% Cache for constraints builder
+mpcData.pred.soc.Phi = Phi_soc; mpcData.pred.soc.G   = G_soc;
+mpcData.pred.v.Phi   = Phi_v;    mpcData.pred.v.G   = G_v;
+mpcData.pred.eta.Phi = Phi_phi;  mpcData.pred.eta.G = G_phi;
+mpcData.pred.u.Cu    = tril(ones(Nc));
+
+% LS-QP unconstrained solution:
+F = -2*G_soc'*(Ref - Phi_soc*dx);
+
+% Adaptive control weighting
 [~,S,~] = svd(G_soc'*G_soc);
 [m,n] = size(S);
-F = (norm(F,2)/(2*mpcData.const.du_max*sqrt(Nc)))-(S(m,n));
-Ru = eye(mpcData.Nc,mpcData.Nc);
+Ru = (norm(F,2)/(2*mpcData.const.du_max*sqrt(Nc)))-(S(m,n));
+Ru = Ru*eye(mpcData.Nc,mpcData.Nc);
 mpcData.Ru = Ru;
 
-E = 2*(Csoc'*G_soc + Ru);
-DU = -E\F;
+E = 2*(G_soc'*G_soc + Ru);
+DU = -E\F; % Unconstrained Future inputs
 
-[M,gamma] = constraintsMPC(dx,cellState,mpcData);
+[M,gamma] = constraintsMPC(dx, mpcData);
 
 %% Check if constraints are violated; if so, run Hildreth
 if sum(M*DU - gamma > 0) > 0
@@ -56,7 +78,7 @@ uk = du + mpcData.uk_1;
 
 mpcData.uk_1 = uk;
 mpcData.xk_1 = xk;
-mpcData.SOCk_1 = cellState.MPC.prior.SOC;
+% mpcData.SOCk_1 = cellState.MPC.prior.SOC;
 mpcData.DUk_1 = DU;
 
 end
